9d46931f1a1c3aa0ec5dfc89905f756174820d11
westy12dan <westy12dan@gmail.com>
2022-04-20 14:42:22 +0800

    Increase public cache time

--- server/index.ts
+++ server/index.ts
@@ -42,8 +42,7 @@ io.on("connection", (socket) => {
 
 app.use(compression());
 
-// You may want to be more aggressive with this caching
-app.use(express.static("public", { maxAge: "1h" }));
+app.use(express.static("public", { maxAge: "1d" }));
 
 // Remix fingerprints its assets so we can cache forever
 app.use(express.static("public/build", { immutable: true, maxAge: "1y" }));
