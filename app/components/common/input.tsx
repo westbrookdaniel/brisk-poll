29aa70f3251dcf216d689423363ba4f012b084ee
westy12dan <westy12dan@gmail.com>
2022-04-23 13:40:31 +0800

    Fix rounded checkboxes

--- app/components/common/input.tsx
+++ app/components/common/input.tsx
@@ -4,7 +4,7 @@ import { noRing, transition, withRing } from "./styles";
 const manualBlockInvalid = `focus:border-red-700 border-red-700`;
 const blockInvalid = `focus:invalid:border-red-700 invalid:border-red-700`;
 const blockFocused = `focus:border-gray-500 focus:bg-white focus:outline-none focus:outline-0`;
-const block = `block rounded-lg bg-gray-100 transition-color ${transition} ${blockInvalid} ${blockFocused}`;
+const block = `block bg-gray-100 transition-color ${transition} ${blockInvalid} ${blockFocused}`;
 
 export const Input: React.FC<React.HTMLProps<HTMLInputElement>> = ({
   className = "",
@@ -15,7 +15,7 @@ export const Input: React.FC<React.HTMLProps<HTMLInputElement>> = ({
     <input
       className={`${block} ${noRing} ${
         invalid ? manualBlockInvalid : "border-transparent"
-      } form-input ${className}`}
+      } form-input rounded-lg ${className}`}
       aria-invalid={invalid}
       {...props}
     />
