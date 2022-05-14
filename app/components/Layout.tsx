d56488c2d18db22f31f577f2fdfa1e6646a4c575
westy12dan <westy12dan@gmail.com>
2022-05-14 14:26:25 +0800

    Update mobile layout

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex flex-col justify-center flex-grow w-full max-w-xl mx-auto space-y-16">
-      <div className="w-full p-4 min-h-xl md:p-8">{children}</div>
+      <div className="w-full p-4 min-h-lg md:min-h-xl md:p-8">{children}</div>
     </main>
   );
 }
